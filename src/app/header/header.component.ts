cf979ea0b3d1fbfc8f4291886247c43a4b763ff0
vishu7512 <vishalgupta7684@gmail.com>
2024-09-05 00:51:20 +0530

    update seller section

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,130 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { product } from '../data-type';
+// import { ProductService } from '../services/product.service';
+// import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+// import { faSearch } from '@fortawesome/free-solid-svg-icons';
+// import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+// import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.css'],
+// })
+// export class HeaderComponent implements OnInit {
+//   menuType: string = 'default';
+//   searchText: string = '';
+//   sellerName:string="";
+//   userName:string="";
+//   searchResult:undefined|product[];
+//   cartItems = 0;
+//   icons = faCartShopping;
+//   icon = faSearch;
+//   user = faUserCircle;
+//   profile = faCaretDown;
+//   isDropdownOpen = false;
+//   // searchtext:any
+//   constructor(private route: Router, private product:ProductService) {}
+
+//   ngOnInit(): void {
+//     this.route.events.subscribe((val: any) => {
+//       if (val.url) {
+//         if (localStorage.getItem('seller') && val.url.includes('seller')) {
+//          let sellerStore=localStorage.getItem('seller');
+//          let sellerData =sellerStore && JSON.parse(sellerStore)[0];
+//          this.sellerName=sellerData.name;
+//           this.menuType = 'seller';
+//         }
+//         else if(localStorage.getItem('user')){
+//           let userStore = localStorage.getItem('user');
+//           let userData = userStore && JSON.parse(userStore);
+//           this.userName= userData.name;
+//           this.menuType='user';
+//           this.product.getCartList(userData.id);
+//         }
+//          else {
+//           this.menuType = 'default';
+//         }
+//       }
+//     });
+//     let cartData = localStorage.getItem('localCart');
+//     if (cartData) {
+//       this.cartItems = JSON.parse(cartData).length;
+//     }
+//     this.product.cartData.subscribe((items) => {
+//       this.cartItems = items.length;
+//     });
+//   }
+//   logout(){
+//     localStorage.removeItem('seller');
+//     this.route.navigate(['/'])
+//   }
+
+//   userLogout(){
+//     localStorage.removeItem('user');
+//     this.route.navigate(['/user-auth'])
+//     this.product.cartData.emit([])
+//   }
+
+
+//  UserProfile() {
+//     const userStore = localStorage.getItem('user');
+//     if (userStore) {
+//       const userData = JSON.parse(userStore);
+//       // Pass user data to the user profile component via state
+//       this.route.navigate(['/user-profile'], { state: { user: JSON.stringify(userData) } });
+//     }
+//   }
+
+// searchProduct(query: KeyboardEvent): void {
+//   if (query) {
+//     const element = query.target as HTMLInputElement;
+//     const searchText = element.value.trim(); // Get the search text
+
+//     if (searchText) {
+//       this.product.searchProduct(searchText).subscribe((result) => {
+//        if(result.length>5){
+//         result.length=5
+//         this.searchResult = result;}
+//       });
+//     } else {
+//       this.searchResult = []; // Clear the search result if input is empty
+//     }
+//   }
+// }
+
+
+//   hideSearch(){
+//     this.searchResult=undefined
+//   }
+//   redirectToDetails(id:number){
+//     this.route.navigate(['/details/'+id])
+//   }
+//   // submitSearch(val:string){
+//   //   console.warn(val)
+//   // this.route.navigate([`search/${val}`]);
+//   // }
+//   submitSearch(event: KeyboardEvent, val: string): void {
+//     if (event.key === 'Enter') {
+//       this.route.navigate([`search/${val}`]);
+//     }
+//   }
+
+//   toggleDropdown() {
+//   this.isDropdownOpen = !this.isDropdownOpen;
+// }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { product } from '../data-type';
 import { ProductService } from '../services/product.service';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,39 +133,28 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent implements OnInit {
   menuType: string = 'default';
   searchText: string = '';
-  sellerName:string="";
-  userName:string="";
-  searchResult:undefined|product[];
+  sellerName: string = '';
+  userName: string = '';
+  searchResult: undefined | product[];
   cartItems = 0;
   icons = faCartShopping;
   icon = faSearch;
   user = faUserCircle;
   profile = faCaretDown;
   isDropdownOpen = false;
-  // searchtext:any
-  constructor(private route: Router, private product:ProductService) {}
+
+  constructor(private route: Router, private product: ProductService) {}
 
   ngOnInit(): void {
-    this.route.events.subscribe((val: any) => {
-      if (val.url) {
-        if (localStorage.getItem('seller') && val.url.includes('seller')) {
-         let sellerStore=localStorage.getItem('seller');
-         let sellerData =sellerStore && JSON.parse(sellerStore)[0];
-         this.sellerName=sellerData.name;
-          this.menuType = 'seller';
-        }
-        else if(localStorage.getItem('user')){
-          let userStore = localStorage.getItem('user');
-          let userData = userStore && JSON.parse(userStore);
-          this.userName= userData.name;
-          this.menuType='user';
-          this.product.getCartList(userData.id);
-        }
-         else {
-          this.menuType = 'default';
-        }
+    this.checkUserOrSeller();
+
+    this.route.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.checkUserOrSeller();
       }
     });
+
+    // Load cart data
     let cartData = localStorage.getItem('localCart');
     if (cartData) {
       this.cartItems = JSON.parse(cartData).length;
@@ -55,55 +163,69 @@ export class HeaderComponent implements OnInit {
       this.cartItems = items.length;
     });
   }
-  logout(){
+
+  checkUserOrSeller(): void {
+    if (localStorage.getItem('seller')) {
+      let sellerStore = localStorage.getItem('seller');
+      let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+      this.sellerName = sellerData.name;
+      this.menuType = 'seller';
+    } else if (localStorage.getItem('user')) {
+      let userStore = localStorage.getItem('user');
+      let userData = userStore && JSON.parse(userStore);
+      this.userName = userData.name;
+      this.menuType = 'user';
+      this.product.getCartList(userData.id);
+    } else {
+      this.menuType = 'default';
+    }
+  }
+
+  logout() {
     localStorage.removeItem('seller');
-    this.route.navigate(['/'])
+    this.route.navigate(['/']);
   }
 
-  userLogout(){
+  userLogout() {
     localStorage.removeItem('user');
-    this.route.navigate(['/user-auth'])
-    this.product.cartData.emit([])
+    this.route.navigate(['/user-auth']);
+    this.product.cartData.emit([]);
   }
 
-
- UserProfile() {
+  UserProfile() {
     const userStore = localStorage.getItem('user');
     if (userStore) {
       const userData = JSON.parse(userStore);
-      // Pass user data to the user profile component via state
       this.route.navigate(['/user-profile'], { state: { user: JSON.stringify(userData) } });
     }
   }
 
-searchProduct(query: KeyboardEvent): void {
-  if (query) {
-    const element = query.target as HTMLInputElement;
-    const searchText = element.value.trim(); // Get the search text
+  searchProduct(query: KeyboardEvent): void {
+    if (query) {
+      const element = query.target as HTMLInputElement;
+      const searchText = element.value.trim();
 
-    if (searchText) {
-      this.product.searchProduct(searchText).subscribe((result) => {
-       if(result.length>5){
-        result.length=5
-        this.searchResult = result;}
-      });
-    } else {
-      this.searchResult = []; // Clear the search result if input is empty
+      if (searchText) {
+        this.product.searchProduct(searchText).subscribe((result) => {
+          if (result.length > 5) {
+            result.length = 5;
+            this.searchResult = result;
+          }
+        });
+      } else {
+        this.searchResult = [];
+      }
     }
   }
-}
 
+  hideSearch() {
+    this.searchResult = undefined;
+  }
 
-  hideSearch(){
-    this.searchResult=undefined
+  redirectToDetails(id: number) {
+    this.route.navigate(['/details/' + id]);
   }
-  redirectToDetails(id:number){
-    this.route.navigate(['/details/'+id])
-  }
-  // submitSearch(val:string){
-  //   console.warn(val)
-  // this.route.navigate([`search/${val}`]);
-  // }
+
   submitSearch(event: KeyboardEvent, val: string): void {
     if (event.key === 'Enter') {
       this.route.navigate([`search/${val}`]);
@@ -111,6 +233,6 @@ searchProduct(query: KeyboardEvent): void {
   }
 
   toggleDropdown() {
-  this.isDropdownOpen = !this.isDropdownOpen;
-}
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
